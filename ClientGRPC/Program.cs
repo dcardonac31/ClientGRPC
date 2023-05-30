74e825704ecfeb5f9f0499a7011227c845577e23
David Sneider Cardona Cardenas <dcardonac@outlook.com>
2023-05-30 15:16:40 -0500

    First commit

---
+++ ClientGRPC/Program.cs
@@ -0,0 +1,21 @@
+﻿using ClientGRPC;
+using Grpc.Net.Client;
+
+using (var channel = GrpcChannel.ForAddress("https://localhost:7047"))
+{
+    var client = new Beers.BeersClient(channel);
+    var reply = await client.SayHelloAsync(new HelloRequest
+    {
+        Name = "Majo"
+    });
+
+    var reply2 = await client.GetAsync(new BeersRequest { });
+
+    Console.WriteLine(reply.Message);
+
+    foreach (var beer in reply2.Beers)
+    {
+        Console.WriteLine(beer);
+    }
+    Console.ReadKey();
+}
